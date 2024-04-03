9a91d716f1416a2f0951809392fd2d678f0d3c19
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2024-04-03 14:57:31 -0400

    Comments and Javadoc. Also tweaked the course format just a little bit in the search.

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -12,7 +12,6 @@ public class Main {
     private static Schedule currentSchedule;
     private static Student currentStudent;
     private static ArrayList<Course> courseCatalog;
-
     private static int schedCount;
 
 
