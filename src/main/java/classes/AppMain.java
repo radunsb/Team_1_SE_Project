fc311c675d70dfcc718347a72d2b480a988a27fe
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2024-05-02 15:21:01 -0400

    Got react and the backend communicating, can make api calls from the frontend.

--- src/main/java/classes/AppMain.java
+++ src/main/java/classes/AppMain.java
@@ -2,11 +2,15 @@ package classes;
 
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 import io.javalin.Javalin;
 import io.javalin.plugin.bundled.CorsPluginConfig;
 
 public class AppMain {
+
+    public static Student user;
+    public static int currentSchedule;
 
     public static void main(String[] args) {
         //Read the CSV
@@ -15,6 +19,20 @@ public class AppMain {
         } catch(FileNotFoundException | ParseException e){
             System.out.println(e.getMessage());
         }
+
+        // Create the student user
+        ArrayList<Major> majors = new ArrayList<>();
+        ArrayList<Minor> minors = new ArrayList<>();
+        ArrayList<Course> courseHistory = new ArrayList<>();
+        ArrayList<Schedule> schedules = new ArrayList<>();
+        Schedule defaultSchedule = new Schedule(0, "FALL", 2020, "mySchedule");
+        Schedule defaultSchedule2 = new Schedule(1, "FALL", 2020, "otherSchedule");
+        schedules.add(defaultSchedule);
+        schedules.add(defaultSchedule2);
+
+        currentSchedule = 0;
+        user = new Student(9999, "newStudent", Student.Class.OTHER, majors, minors, courseHistory, schedules);
+
 
         //Set up Javalin api on port 7979
         Javalin app = Javalin.create(config -> {
@@ -29,6 +47,16 @@ public class AppMain {
         app.get("/search", SearchController::emptySearch);
         app.get("/search/{query}", SearchController::getResults);
         //TODO: add filter endpoints
+
+        app.get("/getScheduleNames", StudentController::getScheduleNames);
+        app.get("/getCurrentSchedule", StudentController::getCurrentSchedule);
+        app.get("/getSchedule/{scheduleName}", StudentController::getScheduleByName);
+
+        app.get("/changeCurrentSchedule/{scheduleName}", StudentController::changeCurrentSchedule);
+
+        // kind of optional
+        //app.post("/changeScheduleName", StudentController::changeScheduleName);
+
 
 
 
--- src/main/react_part/src/index.js
+++ src/main/react_part/src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './Home.css';
 import Home from './Home.js';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<BrowserRouter>
   <React.StrictMode>
     <Home />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
